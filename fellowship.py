515685fa54605aa4f6f0640cec326dff5b492012
Tom Hasselman <thasselm@mix.wvu.edu>
2022-09-15 23:22:23 +0000

    innitial commit

---
+++ fellowship.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import subprocess
+import time
+import datetime
+import enum
+
+class Colors:
+    Red = '\x1b[38;5;124m'
+    Green = '\x1b[38;5;119m'
+    Blue = '\x1b[38;5;122m'
+    White = '\x1b[38;5;231m'
+
+try:
+
+    freq = input('How often should your connection be checked? (seconds)\n')
+    remote = input('What target should be checked for availability?\n')
+
+
+    command = ["ping", "-c", "1", "-w", freq, remote]
+
+    print("Checking " + remote + " every " + freq + " seconds...")
+
+    with open("availability.txt", 'a', encoding = 'utf-8') as log:
+        while True:
+            result = subprocess.call(command, stdout = subprocess.DEVNULL)
+
+            while(result == 0):
+                result = subprocess.call(command, stdout = subprocess.DEVNULL)
+
+                print(Colors.Green + "Internet came online at: " + Colors.White + str(datetime.datetime.now()) +   #ping worked
+                Colors.Blue + "\nWriting to file...")
+                log.write("Internet came online at: " + str(datetime.datetime.now()) + "\n") #logging the date and time that the internet came online
+
+                while(result == 0):
+                    result = subprocess.call(command, stdout = subprocess.DEVNULL)
+
+                    print(Colors.Green + "Internet is still up")
+                    time.sleep(int(freq))
+
+            else:
+                result = subprocess.call(command, stdout = subprocess.DEVNULL)
+
+                print(Colors.Red + "Internet went down at: " + Colors.White + str(datetime.datetime.now()) +  #ping did not work
+                Colors.Blue + "\nWriting to file...")
+                log.write("Internet went offline at: " + str(datetime.datetime.now()) + "\n")
+
+                while(result !=0):
+                    result = subprocess.call(command, stdout = subprocess.DEVNULL)
+                    print(Colors.Red + "Internet is still down")
+                    time.sleep(int(freq))
+except KeyboardInterrupt:
+    print(Colors.White + "\rShutting down...")
